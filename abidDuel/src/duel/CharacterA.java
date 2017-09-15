813e18f398ec18505a9fbe19566089294415aa74
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2017-09-15 15:35:13 -0400

    hui

--- abidDuel/src/duel/CharacterA.java
+++ abidDuel/src/duel/CharacterA.java
@@ -5,12 +5,17 @@ public class CharacterA extends Duel implements Dueler{
 	private int HP;
 	public CharacterA() {
 		 name = "vic";
-		 HP = 10;
 	}
 	public String getName() {
 		return name;
 	}
 	public int getHP() {
 		return HP;
+	}
+	public void setStartingHP(int HP) {
+		HP = 10;
+	}
+	public void taunt() {
+		
 	}
 }
