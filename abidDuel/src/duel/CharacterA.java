d451fa4bacf71f12399ade92234c0487db90ad26
ahossain3678 <ahossain3678@bths.edu>
2017-09-16 23:06:07 -0400

    CharacterA update

--- abidDuel/src/duel/CharacterA.java
+++ abidDuel/src/duel/CharacterA.java
@@ -1,21 +1,38 @@
 package duel;
 
+<<<<<<< Upstream, based on 1ee76262e2dea31d4832c7f41c23805b28c8380b
 public class CharacterA implements Dueler{
+=======
+public class CharacterA implements Dueler {
+>>>>>>> da296af CharacterA update
 	private String name;
 	private int HP;
 	public CharacterA() {
-		 name = "Vic";
+		name = "Riley Sins";
+	}
+	public void taunt() {
+		
 	}
 	public String getName() {
 		return name;
 	}
+	public void setStartingHP(int hp) {
+		HP = hp;	
+	}
 	public int getHP() {
 		return HP;
 	}
-	public void setStartingHP(int HP) {
-		HP = 10;
+	public boolean determineOpponentIsFair(Dueler d, int hp){
+		if(d.getHP() == hp){
+		return true;
+		}else{
+			return false;
+		}
 	}
-	public void taunt() {
+	public int getAction(Object caller) {
 		
+	}
+	public void hit(Object caller) {
+		HP -= 10;
 	}
 }
